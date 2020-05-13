b93d647334ea5fbd2e4e6e7ecd75bb1d52f2a377
Adam Gackenheimer <agack@touchpointone.com>
2020-05-12 22:06:41 -0400

    Add basic bar graph visualization

--- SortVisualizer/MainPage.xaml.cs
+++ SortVisualizer/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,39 @@ namespace SortVisualizer
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            clickedButton.Content = "Clicked";
+            int[] data = {5, 12, 9, 2, 16, 4, 14, 11, 1, 7, 13, 8, 10, 3, 15, 6};
+            createBars(data);
+        }
+
+        private void createBars(int[] data)
+        {
+            Canvas.Children.Clear();
+            double maxWidth = Canvas.Width;
+            double maxHeight = Canvas.Height;
+            double barWidth = maxWidth / data.Length;
+            double heightIncrement = maxHeight / data.Length;
+            for (int i = 0; i < data.Length; i++)
+            {
+                double barHeight = (data[i] * heightIncrement);
+                var bar = new Rectangle();
+                Windows.UI.Color barColor = new Windows.UI.Color();
+                barColor.R = Convert.ToByte((barHeight / maxHeight) * 255);
+                barColor.B = Convert.ToByte(((maxHeight - barHeight) / maxHeight) * 255);
+                barColor.A = 255;
+                Canvas.Children.Add(bar);
+                bar.Name = "Bar" + i;
+                bar.Fill = new SolidColorBrush(barColor);
+                bar.Stroke = new SolidColorBrush(barColor);
+                bar.Margin = new Thickness(i * barWidth, maxHeight - barHeight, 0, 0);
+                bar.Width = barWidth;
+                bar.Height = barHeight;
+            }
         }
     }
 }
